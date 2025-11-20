f533a10a4ddded2ad07be04def8606c1c1eed527
VYNCENTHARRIS <165708653+VYNCENTHARRIS@users.noreply.github.com>
2025-11-20 09:20:27 -0500

    Add files via upload

---
+++ railMiles.js
@@ -0,0 +1,12 @@
+const currentMiles = 19.3; 
+
+export const railMilesData = [
+  {
+    scenario: 'Current',
+    miles: currentMiles,
+  },
+  {
+    scenario: 'Future',
+    miles: currentMiles + 43,
+  },
+];
---
+++ support.js
@@ -0,0 +1,12 @@
+export const supportData = [
+  {
+    key: 'ACCC 2021',
+    label: 'Funding support (ACCC 2021)',
+    percent: 62,
+  },
+  {
+    key: 'TSP 2025',
+    label: 'Want more transit (TSP engagement)',
+    percent: 93,
+  },
+];
